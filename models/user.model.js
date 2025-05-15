0dca32f15ce3fa7d5fc6d09a46a801610ae89770
piyush goyal <piyush2022ug@gmail.com>
2025-05-15 10:35:40 +0530

    feat: user model

---
+++ models/user.model.js
@@ -0,0 +1,54 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      match: [/\S+@\S+\.\S+/, "is invalid"],
+      index: true,
+    },
+    bio: {
+      type: String,
+      default: "",
+    },
+    image: {
+      type: String,
+      default:
+        "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png",
+    },
+    favouriteArticles: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "pg43Article",
+      },
+    ],
+    followingUsers: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "pg43User",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("pg43User", userSchema);
